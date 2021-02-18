cdd38e55a714571d28fb0418dd81503110e46498
Stefano Agostini <stefano.agostini93@gmail.com>
2021-02-18 12:44:49 +0100

    small changes to improve the code

--- js/addToList.js
+++
@@ -1,7 +0,0 @@
-
-function addToList(idProductAtMarket){
-    var quantity = document.getElementById("exampleFormControlSelect1").value
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", "utility/add-to-list.php?productMarketId=" + idProductAtMarket + "&quantity=" + quantity, true);
-    xmlhttp.send();
-}
