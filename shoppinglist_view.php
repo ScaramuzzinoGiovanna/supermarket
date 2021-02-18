f5d145a3d03ba445055d16147d662127108dbb7e
Giovanna Scaramuzzino <scaramuzzinogiovanna95@gmail.com>
2021-02-18 15:46:12 +0100

    remove import file js

--- shoppinglist_view.php
+++ shoppinglist_view.php
@@ -14,67 +14,75 @@ include("utility/list.php");
     <div class="container containerPaddingMargin">
         <h1 class="text-center">Lista della spesa</h1>
         <ul class="list-group">
-            <li class="list-group-item list-group-item-secondary">
-                <div class="row">
-                    <div class="col">
-                        SupermarketName
-                    </div>
-                    <div class="col-auto ">
-                        <p class="text-xl-right">Totale: </p>
-                        <p class="text-xl-right">_totale_</p>
-                    </div>
-                </div>
-            </li>
-            <fieldset>
-                <li class="list-group-item">
-                    <div class="form-group">
+            <?php
+            $lastAddress = "";
+            $lastCity = "";
+            $lastEnterprise = "";
+            foreach($arr as $e){?>
+                 <!-- header list (supermarket)-->
+                 <?php if ($e[2] != $lastAddress or $e[3] != $lastCity or $e[4] != $lastEnterprise){ ?>
+                 </li>
+                     </fieldset>   
+                    <li class="list-group-item list-group-item-secondary">
                         <div class="row">
                             <div class="col">
-                                <div class="custom-control custom-checkbox">
-                                    <input class="custom-control-input" id="customCheck2" type="checkbox" value="">
-                                </div>
+                                <h5> <?php echo $e[4]?></h5>
+                                <br>
+                               <p> <?php echo $e[2] ?> &nbsp <?php echo $e[3]?> </p> 
                             </div>
-                            <div class="col">
-                                <img class="prodImgList" src="img/product/Branzino.png">
-                            </div>
-                            <div class="col">
-                                name_prod quantità_prod prezzo
+                            <div class="col-auto ">
+                                <p class="text-xl-right">Totale: </p>
+                                <p class="text-xl-right">_totale_</p>
                             </div>
                         </div>
-                    </div>
-                </li>
-                </fieldset>
+                    </li>
+                    <fieldset>
+                        <li class="list-group-item">
+                        <div class="form-group">
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="custom-control custom-checkbox">
+                                            <input class="custom-control-input" id="customCheck2" type="checkbox" value="">
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <img class="prodImgList" src="img/product/Branzino.png">
+                                    </div>
+                                    <div class="col">
+                                    <?php echo $e[0] ?>
+                                        name_prod quantità_prod prezzo
+                                    </div>
+                                </div>
+                            </div>
+                 <!-- product list-->
+                     <?php }else{ ?>
+                     
+                        <p> <?php  echo $e[0] ?> </p>
+                            <div class="form-group">
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="custom-control custom-checkbox">
+                                            <input class="custom-control-input" id="customCheck2" type="checkbox" value="">
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <img class="prodImgList" src="img/product/Branzino.png">
+                                    </div>
+                                    <div class="col">
+                                    <?php echo $e[0] ?>
+                                        name_prod quantità_prod prezzo
+                                    </div>
+                                </div>
+                            </div>
+                                 
+                <?php } 
+                    $lastAddress = $e[2];
+                    $lastCity = $e[3];
+                    $lastEnterprise = $e[4]; } ?>
+
+           
 
         </ul>
-        <!-- <div class="card">
-            <div class="jumbotron-fluid">
-                <div class="table">
-                    <table class="table ">
-                        <tbody>
-                            <tr>
-                                <th scope="row">
-                                    <fieldset class="form-group">
-                                        <div class="form-check disabled">
-                                            <label class="form-check-label">
-                                                <input class="form-check-input" type="checkbox" value="" disabled="">
-
-                                            </label>
-                                        </div>
-                                    </fieldset>
-                                </th>
-                                <td> <img class="prodImgList" src="img/product/Branzino.png">
-                                <td>name_prod</td>
-                                <td>quantità prod
-                                </td>
-                                <td>prezzo
-                                </td>
-                                <td><img src="images/trash.svg">
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>-->
     </div>
     </div>
     <?php include("templates/script_template.php"); ?>
