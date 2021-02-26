9998eff3406be101bcddda836738d2f93e03e0eb
Giovanna Scaramuzzino <scaramuzzinogiovanna95@gmail.com>
2021-02-26 11:00:49 +0100

    add modal

--- templates/modals.php
+++ templates/modals.php
@@ -31,3 +31,31 @@
         </div>
     </div>
 </div>
+
+<!-- Modal no logged- no add to list -->
+<div class="modal fade" id="prodNoLoggedModal" tabindex="-1" role="dialog" aria-labelledby="prodNoLoggedModal" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="modal-header w-100 text-center">
+                    <div style="flex-direction:column;">
+                        <h5 class="modal-title"> Non puoi ancora aggiungere elementi alla lista:</h5>
+                    </div>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body w-100 text-center bodyModalLocation">
+
+                    <div>
+                        <a class="btn btn-secondary" href="login_view.php">Accedi
+                        </a>
+                    </div>
+                    <p class="marginTop">oppure</p>
+                    <div>
+                        <a class="btn btn-secondary" href="signin_view.php">Registrati
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
