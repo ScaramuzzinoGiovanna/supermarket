b04ec4f0b27c4f11873adaadf4e01117726fa101
Giovanna Scaramuzzino <scaramuzzinogiovanna95@gmail.com>
2021-03-21 15:39:03 +0100

    modified order phrases home page

--- index.php
+++ index.php
@@ -15,9 +15,9 @@ include("templates/head_template.php");
         <h2 class="title">Catalogo prodotti presenti nei supermercati di Italia</h3>
         <div class='container containerPadding300'>
         <h6 class="text-left pb-3"> Per ogni prodotto puoi osservare il supermercato con il prezzo più conveniente. </h6> 
+        <p class="text-left">  <img clas src ="images/geo-alt.svg" alt="geolocalizzazione">  &nbsp Localizzati per trovare i prodotti disponibili nella tua zona. 
         <p class="text-left"> <img clas src ="images/search-black.svg" alt="cerca"> &nbsp Cerca all'interno del nostro catalogo i prodotti
             desiderati. 
-        <p class="text-left">  <img clas src ="images/geo-alt.svg" alt="geolocalizzazione">  &nbsp Localizzati per trovare i prodotti disponibili nella tua zona. 
         <p class="text-left"> <img class src="images/plus-circle.svg" alt="aggiungi prodotto">  &nbsp Aggiungi i prodotti alla lista della spesa. È necassaria la registrazione al sito.</p>
         <div>
     </div>
