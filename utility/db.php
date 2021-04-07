20aba4a0fec42a06e479dbfc8cb1eff8b9ea1e2c
Stefano Agostini <stefano.agostini93@gmail.com>
2021-04-07 21:30:07 +0200

    fix

--- utility/db.php
+++ utility/db.php
@@ -1,8 +1,8 @@
 <?php
-    $servername='localhost';
-    $username='spesaconveniente';
+    $servername='';
+    $username='';
     $db_password='';
-    $dbname = "my_spesaconveniente";
+    $dbname = "";
     $con=mysqli_connect($servername,$username,$db_password,"$dbname");
       if(!$con){
           die('Impossibile connettersi');
